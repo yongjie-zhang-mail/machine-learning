e5c6e5d11ec75b499ad6652044898c443d79f328
yongjie <yongjie_zhang_mail@qq.com>
2022-02-22 19:49:01 +0800

    更新线性模型-梯度下降方式

--- linear-model/LinearModel2.py
+++ linear-model/LinearModel2.py
@@ -7,7 +7,7 @@
 """
 
 from sklearn.datasets import load_boston
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, SGDRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -18,6 +18,34 @@ def linear_model1():
     正规方程
     :return: None
     """
+    x_test, x_train, y_test, y_train = prepare()
+
+    # 机器学习（线性回归）
+    estimator = LinearRegression()
+    estimator.fit(x_train, y_train)
+    print("这个模型的偏置是：\n", estimator.intercept_)
+
+    assess(estimator, x_test, y_test)
+
+
+def linear_model2():
+    """
+    梯度下降
+    :return: None
+    """
+    x_test, x_train, y_test, y_train = prepare()
+
+    # 机器学习（线性回归）
+    estimator = SGDRegressor(max_iter=1000)
+    estimator.fit(x_train, y_train)
+    print("这个模型的偏置是：\n", estimator.intercept_)
+
+    assess(estimator, x_test, y_test)
+
+
+def prepare():
+    """准备数据，数据集划分，特征工程
+    """
     # 获取数据
     boston = load_boston()
     # 数据基本处理（数据集划分）
@@ -26,9 +54,12 @@ def linear_model1():
     transfer = StandardScaler()
     x_train = transfer.fit_transform(x_train)
     x_test = transfer.fit_transform(x_test)
-    # 机器学习（线性回归）
-    estimator = LinearRegression()
-    estimator.fit(x_train, y_train)
+    return x_test, x_train, y_test, y_train
+
+
+def assess(estimator, x_test, y_test):
+    """模型评估
+    """
     # 模型评估
     y_pred = estimator.predict(x_test)
     print("预测值：\n", y_pred)
@@ -40,3 +71,4 @@ def linear_model1():
 
 if __name__ == '__main__':
     linear_model1()
+    linear_model2()
