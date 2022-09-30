4243bcab654cb674e600988d1ccdf36b977f22f5
yongjie <yongjie_zhang_mail@qq.com>
2022-09-30 17:38:19 +0800

    更新 模型构造 my

---
+++ d2l-my/model-construction-my.py
@@ -0,0 +1,13 @@
+from torch import nn
+from torch.nn import functional as F
+
+
+# 使用 torch.nn.Module 定义 MLP
+class MLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.hidden = nn.Linear(20, 256)
+        self.out = nn.Linear(256, 10)
+
+    def forward(self, X):
+        return self.out(F.relu(self.hidden(X)))
