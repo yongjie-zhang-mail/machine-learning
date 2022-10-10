029d0ab6ffdd9827deca86d8f50b6a04999f62ac
yongjie <yongjie_zhang_mail@qq.com>
2022-10-10 19:03:57 +0800

    更新 LeNet my

---
+++ d2l-my/lenet-my.py
@@ -0,0 +1,15 @@
+from torch import nn
+
+
+class LeNet():
+    def __init__(self):
+        super().__init__()
+
+    def test1(self):
+        print(1)
+        nn.Conv2d()
+
+
+if __name__ == '__main__':
+    leNet = LeNet()
+    leNet.test1()
