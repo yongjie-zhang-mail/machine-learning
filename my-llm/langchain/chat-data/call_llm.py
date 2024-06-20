2678cf4064c8d0d65a7b37360fd866839fbb7983
zhangyj21 <zhangyj21@lenovo.com>
2024-06-20 18:17:54 +0800

    11

---
+++ my-llm/langchain/chat-data/call_llm.py
@@ -0,0 +1,76 @@
+
+# pip install langchain_openai
+
+
+
+import os
+from langchain_openai import ChatOpenAI
+
+
+class CallLLM:
+
+    def __init__(self):
+        self.text2 = "abcdefghijklmnopqrstuvwxyzabcdefg"
+
+
+    def test(self):
+        print('test')
+
+
+
+
+if __name__ == '__main__':
+    # print("Hello, World!")
+    callLLM = CallLLM()
+    callLLM.test()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
