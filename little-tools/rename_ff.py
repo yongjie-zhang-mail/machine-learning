45d1bc2742d84e73b7f11e3a070fd6b485708da8
yongjie <yongjie_zhang_mail@qq.com>
2024-07-26 00:44:44 +0800

    modify rename_ff.py

---
+++ little-tools/rename_ff.py
@@ -0,0 +1,73 @@
+import os
+
+
+class RenameFF:
+
+    def __init__(self):
+        pass
+
+    def rename_folders(self, path):
+        """
+        Renames folders in the specified path by removing the characters before the '@' symbol in the folder name.
+
+        Args:
+            path (str): The path to the directory containing the folders to be renamed.
+
+        Returns:
+            None
+        """
+        # 遍历指定路径下的所有文件和文件夹
+        for filename in os.listdir(path):
+            # 检查是否为文件夹
+            if os.path.isdir(os.path.join(path, filename)):
+                # 查找 @ 符号
+                at_pos = filename.find('@')
+                if at_pos != -1:
+                    # 提取 @ 之后的部分
+                    new_name = filename[at_pos + 1:]
+                    # 构建新的路径
+                    new_path = os.path.join(path, new_name)
+                    # 重命名文件夹
+                    os.rename(os.path.join(path, filename), new_path)
+                    print(f"Renamed '{filename}' to '{new_name}'")
+
+
+    def rename_files(self, path):
+        """
+        Renames files in the specified directory by removing the characters before the '@' symbol in the filename.
+
+        Args:
+            path (str): The path to the directory containing the files to be renamed.
+
+        Returns:
+            None
+        """
+        # 遍历指定目录
+        for filename in os.listdir(path):
+            if '@' in filename:
+                # 提取 '@' 之后的部分
+                new_filename = filename.split('@', 1)[1]
+                
+                # 构建完整的路径
+                old_filepath = os.path.join(path, filename)
+                new_filepath = os.path.join(path, new_filename)
+                
+                # 重命名文件
+                os.rename(old_filepath, new_filepath)
+                print(f"Renamed '{filename}' to '{new_filename}'")
+
+
+
+if __name__ == '__main__':
+    rff = RenameFF()
+    # 调用函数，传入你想要遍历的目录路径
+    # rff.rename_folders('/path/to/your/directory')  
+    # rff.rename_folders('D:\download8')
+
+    # 调用函数
+    rff.rename_files(r'C:\download1\temp2')
+
+    
+    
+
+
