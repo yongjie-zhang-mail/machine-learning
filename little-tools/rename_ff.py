230f302aafcf54d3f5228a25ccaebae176170bf0
yongjie <yongjie_zhang_mail@qq.com>
2024-07-26 01:27:38 +0800

    modify rename_ff.py

--- little-tools/rename_ff.py
+++ little-tools/rename_ff.py
@@ -57,16 +57,66 @@ class RenameFF:
                 print(f"Renamed '{filename}' to '{new_filename}'")
 
 
+    def rename_files_current_dir(self):
+        """
+        Renames files in the current directory by removing the characters before the '@' symbol in the filename.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        # 遍历当前目录
+        for filename in os.listdir():
+            if '@' in filename:
+                # 提取 '@' 之后的部分
+                new_filename = filename.split('@', 1)[1]
+                
+                # 构建完整的路径
+                old_filepath = os.path.join(os.getcwd(), filename)
+                new_filepath = os.path.join(os.getcwd(), new_filename)
+                
+                # 重命名文件
+                os.rename(old_filepath, new_filepath)
+                print(f"Renamed '{filename}' to '{new_filename}'")
+
+    
+    def rename_files_current_dir_recursive(self):
+        """
+        Renames files in the current directory and all its subdirectories by removing the characters before the '@' symbol in the filename.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        # 遍历当前目录
+        for root, dirs, files in os.walk(os.getcwd()):
+            for filename in files:
+                if '@' in filename:
+                    # 提取 '@' 之后的部分
+                    new_filename = filename.split('@', 1)[1]
+                    
+                    # 构建完整的路径
+                    old_filepath = os.path.join(root, filename)
+                    new_filepath = os.path.join(root, new_filename)
+                    
+                    # 重命名文件
+                    os.rename(old_filepath, new_filepath)
+                    print(f"Renamed '{filename}' to '{new_filename}'")
+
 
 if __name__ == '__main__':
     rff = RenameFF()
     # 调用函数，传入你想要遍历的目录路径
     # rff.rename_folders('/path/to/your/directory')  
     # rff.rename_folders('D:\download8')
-
     # 调用函数
-    rff.rename_files(r'C:\download1\temp2')
-
+    # rff.rename_files(r'C:\download1\temp2')
+    # rff.rename_files_current_dir()
+    rff.rename_files_current_dir_recursive()
     
     
 
