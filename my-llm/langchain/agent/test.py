989c25914e999ce8df6b40f5c383a67ecfa8b800
yongjie <yongjie_zhang_mail@qq.com>
2024-07-28 12:17:07 +0800

    modified:   my-llm/langchain/agent/test.py

--- my-llm/langchain/agent/test.py
+++ my-llm/langchain/agent/test.py
@@ -13,14 +13,17 @@ from langchain.schema.output_parser import StrOutputParser
 import dashscope
 from dashscope import TextEmbedding
 
-
-
 # pip install --upgrade langchain-community "docarray"
 # from langchain.vectorstores import DocArrayInMemorySearch
 from langchain_community.vectorstores import DocArrayInMemorySearch
 
 # from langchain.embeddings import DashScopeEmbeddings
 from langchain_community.embeddings import DashScopeEmbeddings
+
+from langchain.schema.runnable import RunnableMap
+
+
+
 
 
 class AgentTest:
@@ -74,14 +77,73 @@ class AgentTest:
             ["哈里森在肯肖工作", "熊喜欢吃蜂蜜"],
             embedding=embeddings
         )
-        # query = "哈里森在哪里工作？"
-        query = "熊喜欢吃什么？"
+        query = "哈里森在哪里工作？"
+        # query = "熊喜欢吃什么？"
         # result = db.similarity_search(query)
         
         retriever = db.as_retriever()
         result = retriever.invoke(query)
 
         print(result)
+
+
+    def runnable_map_test(self, message):
+
+        # 创建一个 ChatTongyi 模型实例
+        model = ChatTongyi()
+        # 创建一个StrOutputParser实例，用于解析输出
+        output_parser = StrOutputParser()
+
+        embeddings = DashScopeEmbeddings()
+        db = DocArrayInMemorySearch.from_texts(
+            ["哈里森在肯肖工作", "熊喜欢吃蜂蜜"],
+            embedding=embeddings
+        )                
+        retriever = db.as_retriever()
+
+        rm = RunnableMap({
+            "context": lambda x: retriever.invoke(x["question"]),
+            "question": lambda x: x["question"]
+        })
+        result = rm.invoke({"question": message})
+        print(result)
+        
+        # 定义一个模板字符串template
+        template = """仅根据以下上下文回答问题：
+        {context}
+
+        问题：{question}
+        """
+        # 使用 template 作为模板
+        prompt = ChatPromptTemplate.from_template(template)
+
+        chain = rm | prompt | model | output_parser
+
+        result = chain.invoke({"question": message})
+        # result = chain.invoke({"question": })
+        print(result)      
+
+
+    def bind_function(self): 
+        # 定义一个函数
+        functions = [
+            {
+                "name": "get_current_weather",
+                "description": "获取指定位置的当前天气情况",
+                "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                    "type": "string",
+                    "description": "城市和省份，例如：北京，北京市",
+                    },
+                    "unit": {"type": "string", "enum": ["摄氏度", "华氏度"]},
+                },
+                "required": ["location"],
+                },
+            }
+        ]
+
 
 
 
@@ -93,7 +155,10 @@ if __name__ == '__main__':
     # agentTest.test_stream()
     # agentTest.simple_chain()
     # agentTest.generate_embeddings('AI PC 是指什么？')
-    agentTest.complex_chain()
+    # agentTest.complex_chain()
+    # 测试问题："哈里森在哪里工作？", "熊喜欢吃什么？"
+    # message = "哈里森在哪里工作？"
+    # agentTest.runnable_map_test(message=message)
 
 
 
