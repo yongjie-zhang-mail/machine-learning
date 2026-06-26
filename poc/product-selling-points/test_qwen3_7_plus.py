f4cedd2b6166a45764a99baa1b122600f650c2fb
yongjie <yongjie_zhang_mail@qq.com>
2026-06-26 20:53:46 +0800

    update(ml): update model output part, better streaming.

--- poc/product-selling-points/test_qwen3_7_plus.py
+++ poc/product-selling-points/test_qwen3_7_plus.py
@@ -83,32 +83,48 @@ messages = [
 ]
 
 
+enable_thinking = False  # 是否启用思考过程
+reasoning_content = ""  # 定义完整思考过程
+answer_content = ""     # 定义完整回复
+is_answering = False   # 判断是否结束思考过程并开始回复
+
 completion = client.chat.completions.create(
     model="qwen3.7-plus",  # 您可以按需更换为其它深度思考模型
     messages=messages,
-    extra_body={"enable_thinking": True},  # 是否启用思考过程
-    stream=True,
-    # stream_options = {
-    #     "include_usage": True,  # 是否包含使用量信息
-    # }
+    # 是否启用思考过程
+    extra_body = {
+        "enable_thinking": enable_thinking
+    },  
+    stream=True
 )
 
-is_answering = False  # 是否进入回复阶段
-print("\n" + "=" * 20 + "思考过程" + "=" * 20)
+if enable_thinking:
+    print("\n" + "=" * 20 + "思考过程" + "=" * 20 + "\n")
+
 for chunk in completion:
+    # 如果chunk.choices为空，则打印usage
     if not chunk.choices:
-        continue
-    delta = chunk.choices[0].delta
-    if hasattr(delta, "reasoning_content") and delta.reasoning_content is not None:
-        if not is_answering:
-            print(delta.reasoning_content, end="", flush=True)
-    if hasattr(delta, "content") and delta.content:
-        if not is_answering:
-            print("\n" + "=" * 20 + "回答内容" + "=" * 20)
-            is_answering = True
-        print(delta.content, end="", flush=True)
+        print("\nUsage:")
+        print(chunk.usage)
+    else:
+        delta = chunk.choices[0].delta
+        # 打印思考过程
+        if hasattr(delta, 'reasoning_content') and delta.reasoning_content is not None:
+            print(delta.reasoning_content, end='', flush=True)
+            reasoning_content += delta.reasoning_content
+        else:
+            # 开始回复
+            if delta.content != "" and is_answering is False:
+                print("\n" + "=" * 20 + "回答内容" + "=" * 20 + "\n")
+                is_answering = True
+            # 打印回复过程
+            print(delta.content, end='', flush=True)
+            answer_content += delta.content
 
 
 
-
+# print("=" * 20 + "完整思考过程" + "=" * 20 + "\n")
+# print(reasoning_content)
+# print("=" * 20 + "完整回答内容" + "=" * 20 + "\n")
+# print(answer_content)
 
